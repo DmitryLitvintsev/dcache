6b4a4c3d3343045eb8d0a3b5504b300634cbcfa8
Dmitry Litvintsev <litvinse@fnal.gov>
2019-02-27 00:50:35 -0600

    vo-group plugin: use LinkedHashMap to store glob matches to have predictable behavior when handling multiple matches
    Target: master
    Request: 5.0
    Request: 4.2
    
    Require-book: no
    Require-notes: no

--- modules/gplazma2-fermi/src/main/java/org/dcache/gplazma/plugins/FileBackedVOGroupMap.java
+++ modules/gplazma2-fermi/src/main/java/org/dcache/gplazma/plugins/FileBackedVOGroupMap.java
@@ -73,6 +73,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.dcache.gplazma.AuthenticationException;
@@ -90,9 +91,10 @@ public class FileBackedVOGroupMap {
     private final Map<String, VOGroupEntry> cache = new HashMap<>();
     /**
      * the vo group map file may contain wildcard FQAN match patterns,
-     * allow second cache for these for efficiency
+     * allow second cache for these for efficiency. LinkedHashMap preserves
+     * insertion order.
      */
-    private final Map<Glob, VOGroupEntry> globCache = new HashMap<>();
+    private final Map<Glob, VOGroupEntry> globCache = new LinkedHashMap<>();
     private final File file;
     private final Path path;
     private long lastModified;
