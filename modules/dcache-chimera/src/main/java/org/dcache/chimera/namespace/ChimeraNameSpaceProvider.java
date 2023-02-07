bdc323f28490c4fe6ec4ea5b5ea4d0624183cb8f
Dmitry Litvintsev <litvinse@fnal.gov>
2023-02-07 15:29:33 -0600

    namespace: mv workaround, allow mv between direcrories having none storageclass

--- modules/dcache-chimera/src/main/java/org/dcache/chimera/namespace/ChimeraNameSpaceProvider.java
+++ modules/dcache-chimera/src/main/java/org/dcache/chimera/namespace/ChimeraNameSpaceProvider.java
@@ -678,13 +678,22 @@ public class ChimeraNameSpaceProvider
                             sourceAttributes =
                                   getFileAttributes(new ExtendedInode(_fs, inode), attributes);
                         }
-                        if (!(nullToEmpty(destDirAttributes.getStorageClass()).
-                              equals(nullToEmpty(sourceAttributes.getStorageClass())) &&
-                              nullToEmpty(destDirAttributes.getCacheClass()).
-                                    equals(nullToEmpty(sourceAttributes.getCacheClass())))) {
-                            throw new PermissionDeniedCacheException("Mv denied: " +
-                                  dest.getParent() +
-                                  " has different storage tags; use cp.");
+                        String destStorageClass = nullToEmpty(destDirAttributes.getStorageClass());
+                        String srcStorageClass  = nullToEmpty(sourceAttributes.getStorageClass());
+
+                        if (!srcStorageClass.equals("none.none") &&
+                            !destStorageClass.equals("none.none") &&
+                            !srcStorageClass.equals("") &&
+                            !destStorageClass.equals("")) {
+
+                            if (!(nullToEmpty(destDirAttributes.getStorageClass()).
+                                  equals(nullToEmpty(sourceAttributes.getStorageClass())) &&
+                                  nullToEmpty(destDirAttributes.getCacheClass()).
+                                  equals(nullToEmpty(sourceAttributes.getCacheClass())))) {
+                                throw new PermissionDeniedCacheException("Mv denied: " +
+                                                                         dest.getParent() +
+                                                                         " has different storage tags; use cp.");
+                            }
                         }
                     }
                 }
