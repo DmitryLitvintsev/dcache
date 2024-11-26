10889634642447d03532e01e04f184591b7c3a60
Dmitry Litvintsev <litvinse@fnal.gov>
2024-11-26 15:39:22 -0600

    Revert "bulk api: handle relative paths"
    This reverts commit 3a47f42bd3d8db211a4c9d81e672a14c80133f1a.

--- modules/dcache-frontend/src/test/java/org/dcache/restful/resources/bulk/BulkRequestJsonParseTest.java
+++ modules/dcache-frontend/src/test/java/org/dcache/restful/resources/bulk/BulkRequestJsonParseTest.java
@@ -61,7 +61,6 @@ package org.dcache.restful.resources.bulk;
 
 import static org.dcache.restful.resources.bulk.BulkResources.toBulkRequest;
 import static org.junit.Assert.assertEquals;
-import static diskCacheV111.util.FsPath.create;
 
 import diskCacheV111.util.CacheException;
 import diskCacheV111.util.PnfsHandler;
@@ -183,6 +182,6 @@ public class BulkRequestJsonParseTest {
     }
 
     private void whenParsed() {
-        bulkRequest = toBulkRequest(requestJson, null, handler, create("/"));
+        bulkRequest = toBulkRequest(requestJson, null, handler);
     }
 }
