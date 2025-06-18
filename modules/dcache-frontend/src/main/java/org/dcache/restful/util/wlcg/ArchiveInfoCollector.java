c901e4958e8162e077abafa4648767fb258d2100
Dmitry Litvintsev <litvinse@fnal.gov>
2025-06-18 14:10:01 -0500

    dcache tape rest API: return ONLINE (or DISK) locality for incomplete files
    Motivation:
    ----------
    
    CERN FTS won't cleanup files with locality != DISK. StorRM and
    EOS implementation report locality of these files as DISK
    
    Modification:
    -------------
    
    Return locality ONLINE if locality is NONE for files w/ zero size
    
    Result:
    ------
    
    CERN FTS is happy(ier)
    
    Target: trunk
    Request: 11.0
    Request: 10.2
    Request: 10.1
    Request: 10.0
    Request: 9.2
    
    Patch: https://rb.dcache.org/r/14439/
    Acked-by: Tigran

--- modules/dcache-frontend/src/main/java/org/dcache/restful/util/wlcg/ArchiveInfoCollector.java
+++ modules/dcache-frontend/src/main/java/org/dcache/restful/util/wlcg/ArchiveInfoCollector.java
@@ -60,6 +60,7 @@ documents or software obtained from this server.
 package org.dcache.restful.util.wlcg;
 
 import static com.google.common.util.concurrent.Uninterruptibles.getUninterruptibly;
+import static org.dcache.namespace.FileType.REGULAR;
 
 import com.google.common.base.Throwables;
 import diskCacheV111.util.CacheException;
@@ -165,6 +166,16 @@ public class ArchiveInfoCollector implements CellCommandListener {
     private FileLocality getInfo(String path, PnfsHandler pnfsHandler) throws CacheException {
         FileAttributes attributes = pnfsHandler.getFileAttributes(path, REQUIRED_ATTRIBUTES,
               ACCESS_MASK, false);
-        return poolMonitor.getFileLocality(attributes, "localhost");
+        FileLocality locality = poolMonitor.getFileLocality(attributes, "localhost");
+        /**
+         * This is done to placate CERN FTS that refuses
+         * to remove incomplete files if their locality is not ONLINE
+         */
+        if (attributes.getFileType() == REGULAR && locality == FileLocality.NONE) {
+            if (!attributes.isDefined(FileAttribute.SIZE) || attributes.getSize() == 0) {
+                locality = FileLocality.ONLINE;
+            }
+        }
+        return locality;
     }
 }
